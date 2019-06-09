0d11e349dd818ee6f682f637ee6c1702ce2fd7b5
Douglas Koerich <douglas.koerich@utp.br>
2019-06-08 22:50:06 -0300

    Implementacao do tratamento de colisoes pelo encadeamento INTERIOR.
    Signed-off-by: Douglas Koerich <douglas.koerich@utp.br>

--- hashing/hash.c
+++ hashing/hash.c
@@ -10,16 +10,22 @@ enum Estado {
 };
 typedef enum Estado Estado;
 
+// Definicao de fim da sequencia encadeada de chaves sinonimas
+#define FIM_SINONIMAS -1
+
 // Definicao de uma entrada da tabela de hashing
 struct Entrada {
     R dado;         // o registro armazenado
     Estado estado;  // se existe ou nao um registro nesta entrada da tabela
+    int sinonima;   // indice de uma chave sinonima que pode estar na regiao extra da tabela
 };
 typedef struct Entrada Entrada;
 
 struct Hash {
+    Entrada* vetor; // vetor de entradas (estado + dado + indice-da-sinonima)
     size_t M;       // tamanho do vetor
-    Entrada* vetor; // vetor de entradas (estado + dado)
+    Entrada* extra; // vetor extra de entradas (para as chaves sinominas)
+    size_t P;       // tamanho do extra (pode aumentar a medida das colisoes)
 };
 
 // Implementacao da funcao de hashing (a que vai definir o indice no vetor
@@ -37,69 +43,145 @@ static int h(unsigned chave, size_t tamanho) { // 1o. parametro eh a chave do re
 
 Hash* cria(size_t tamanho) {
     Hash* tabela = malloc(sizeof(Hash));
-    tabela->M = tamanho;
+    tabela->M = tabela->P = tamanho;
 
     // Usa calloc porque ela inicializa a memoria com zeros, o que vai acabar
     // "armazenando" LIVRE (0) em todas as entradas :-) ...
 
     tabela->vetor = calloc(tamanho, sizeof(Entrada));
+    tabela->extra = calloc(tamanho, sizeof(Entrada)); // tamanho extra "a gosto"
 
+    // Indicar que todas as entradas estao com suas ligacoes com uma proxima
+    // chave sinonima como invalidas
+    int i;
+    for (i = 0; i < tamanho; ++i) {
+        tabela->vetor[i].sinonima = tabela->extra[i].sinonima = FIM_SINONIMAS;
+    }
     return tabela;
 }
 
 void destroi(Hash* tabela) {
+    free(tabela->extra);
     free(tabela->vetor);
     free(tabela);
 }
 
-bool inserir(Hash* tabela, const R* registro) {
+void inserir(Hash* tabela, const R* registro) {
     int onde = h(registro->chave, tabela->M); // usa a funcao de hashing para
                                               // definir o local no vetor
-    if (tabela->vetor[onde].estado == OCUPADA) {
-        fprintf(stderr, "Posicao %d ocupada, insercao da chave %u abortada.\n",
-                onde, registro->chave);
-        return false;
+    Entrada* pEntrada = &tabela->vetor[onde];
+    if (pEntrada->estado == OCUPADA) {
+        // Tratamento de colisoes pela estrategia de encadeamento INTERIOR
+
+        while (pEntrada->sinonima != FIM_SINONIMAS) { // equivalente ah busca pelo endereco
+                                                      // NULL que faziamos na lista encadeada
+            pEntrada = &tabela->extra[pEntrada->sinonima]; // aponta pra proxima entrada
+            if (pEntrada->estado == LIVRE) { // encontrei uma posicao que foi liberada por
+                                             // uma chave sinonima antiga ao ser removida
+                break;
+            }
+        }
+        if (pEntrada->estado == OCUPADA) { // terminou o laco sem achar uma livre
+            // Ao chegar neste ponto, estou na ultima entrada da sequencia de sinonimas;
+            // preciso encontrar uma posicao totalmente livre no vetor extra para usar
+            int i;
+            for (i = 0; i < tabela->P; ++i) {
+                if (tabela->extra[i].estado == LIVRE &&
+                    tabela->extra[i].sinonima == FIM_SINONIMAS) { // nao vou ocupar uma posicao
+                                                                  // liberada por outra chave
+                    break;
+                }
+            }
+            if (i == tabela->P) {
+                // Ao percorrer TODO o vetor extra nao se encontrou sequer uma posicao totalmente
+                // livre; entao, realocamos o vetor extra com um tamanho adicional
+                tabela->P *= 2; // dobramos o tamanho da area extra a cada lotacao desse vetor
+                tabela->extra = realloc(tabela->extra, sizeof(Entrada)*tabela->P);
+                int j;
+                for (j = i; j < tabela->P; ++j) {
+                    memset(&tabela->extra[j], 0, sizeof(Entrada)); // zera a nova entrada
+                    tabela->extra[j].sinonima = FIM_SINONIMAS;
+                }
+            }
+            // Vou comecar a ocupar a area expandida a partir do primeiro indice livre
+            pEntrada->sinonima = i;
+            pEntrada = &tabela->extra[i];
+        }
     }
-    memcpy(&tabela->vetor[onde].dado, registro, sizeof(R));
-    tabela->vetor[onde].estado = OCUPADA;
-    return true;
+    memcpy(&pEntrada->dado, registro, sizeof(R));
+    pEntrada->estado = OCUPADA;
 }
 
-int busca(const Hash* tabela, unsigned chave) {
+// Ao inves de retornar o indice como antes, precisa retornar o ponteiro
+// do registro, uma vez que ele pode estar em vetores diferentes ao longo da busca
+
+R* busca(const Hash* tabela, unsigned chave) {
     int onde = h(chave, tabela->M); // usa a funcao de hashing pra recuperar o
                                     // local dessa chave na tabela
-    if (tabela->vetor[onde].estado == LIVRE) {
-        return -1; // chave nao encontrada
+    Entrada* pEntrada = &tabela->vetor[onde];
+
+    // Enquanto estiver na sequencia de chaves sinonimas (mesmo considerando as
+    // liberadas que tem proximas)
+    while ( pEntrada->estado == OCUPADA ||
+           (pEntrada->estado == LIVRE && pEntrada->sinonima != FIM_SINONIMAS)) {
+
+        // Existe registro nesta entrada?
+        if (pEntrada->estado == OCUPADA && pEntrada->dado.chave == chave) {
+            return &pEntrada->dado;
+        }
+        pEntrada = &tabela->extra[pEntrada->sinonima];
     }
-    if (tabela->vetor[onde].dado.chave != chave) {
-        return -1; // outro registro na posicao
-    }
-    return onde;
+    return NULL;
 }
 
 bool remover(Hash* tabela, R* destino, unsigned chave) {
     int onde = h(chave, tabela->M);
-    if (tabela->vetor[onde].estado == LIVRE) {
-        return false; // nao removeu nada (nao havia nada pra remover)
+    Entrada* pEntrada = &tabela->vetor[onde];
+    Entrada* pAnterior = NULL; // vai lembrar quem era a chave anterior para o caso de
+                               // remocao do fim da sequencia (lista) de sinonimas
+
+    // Enquanto estiver na sequencia de chaves sinonimas (mesmo considerando as
+    // liberadas que tem proximas)
+    while ( pEntrada->estado == OCUPADA ||
+           (pEntrada->estado == LIVRE && pEntrada->sinonima != FIM_SINONIMAS)) {
+
+        // Existe registro nesta entrada?
+        if (pEntrada->estado == OCUPADA && pEntrada->dado.chave == chave) {
+            // Encontrei a chave, vou remove-la da tabela
+            memcpy(destino, &pEntrada->dado, sizeof(R));
+            pEntrada->estado = LIVRE;
+
+            // Se estiver removendo do FIM da sequencia (e nao do meio), significa
+            // que vai se tornar uma entrada "totalmente" livre, disponivel inclusive
+            // para outras chaves nao-sinonimas a esta; entao, precisa interromper a
+            // sequencia na entrada anterior
+            if (pEntrada->sinonima == FIM_SINONIMAS && pAnterior != NULL) {
+                pAnterior->sinonima = FIM_SINONIMAS;
+            }
+            return true;
+        }
+        pAnterior = pEntrada;
+        pEntrada = &tabela->extra[pEntrada->sinonima];
     }
-    if (tabela->vetor[onde].dado.chave != chave) {
-        return false; // o registro armazenado nao pode ser removido porque
-                      // a sua chave de busca eh outra
-    }
-    memcpy(destino, &tabela->vetor[onde].dado, sizeof(R));
-    tabela->vetor[onde].estado = LIVRE;
+    return false;
 }
 
 void debug(const Hash* tabela) {
-    system("clear"); // limpa a tela
     int i;
     for (i=0; i<tabela->M; ++i) {
-        // Transforma a chave (numero binario no registro) em uma string
-        char str_chave[16];
-        snprintf(str_chave, sizeof(str_chave), "%u", tabela->vetor[i].dado.chave);
-
-        printf("[%d] {%c} %s\n", i, (tabela->vetor[i].estado == LIVRE? 'L': 'O'),
-               (tabela->vetor[i].estado == LIVRE? "----": str_chave));
+        Entrada* pEntrada = &tabela->vetor[i];
+        printf("[%d] ", i);
+        while (pEntrada != NULL) {
+            if (pEntrada->estado == OCUPADA) {
+                printf("{%5u} ", pEntrada->dado.chave);
+            } else {
+                printf("{livre} ");
+            }
+            printf("--> [%d] ", pEntrada->sinonima);
+            pEntrada = pEntrada->sinonima != FIM_SINONIMAS?
+                       &tabela->extra[pEntrada->sinonima] : NULL;
+        }
+        putchar('\n');
     }
 }
 
--- hashing/hash.h
+++ hashing/hash.h
@@ -12,13 +12,12 @@ typedef struct Hash Hash; // estrutura OPACA que implementa a tabela de hashing
 // Interface de operacoes do TAD da tabela de hashing
 Hash* cria(size_t); // indica o tamanho da tabela a ser criada
 void destroi(Hash*);
-bool inserir(Hash*, const R*);      // le do ponteiro para a tabela de hashing
-                                    // (retorna false se posicao estava ocupada)
+void inserir(Hash*, const R*);      // le do ponteiro para a tabela de hashing
 bool remover(Hash*, R*, unsigned);  // salva no ponteiro o que remove da tabela
                                     // com base na chave procurada
                                     // (retorna false se chave nao encontrada)
-int busca(const Hash*, unsigned);   // retorna o indice da posicao onde estah a chave
-                                    // (ou -1 se nao encontrada)
+R* busca(const Hash*, unsigned);    // retorna o endereco do registro onde estah a chave
+                                    // (ou NULL se nao encontrada)
 
 void debug(const Hash*); // imprime a tabela de hashing com seu conteudo atual
 
