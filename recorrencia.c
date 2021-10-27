cc377df366dd8069dcff0aa9712d7a8d98665d16
Prof. Douglas Koerich <douglas.koerich@utp.br>
2021-10-26 22:29:25 -0300

    Recorrencia geometrica (corrigida)

--- recorrencia.c
+++ recorrencia.c
@@ -77,19 +77,57 @@ T(n) = <
 #endif
 
 int maxVetor_(const int A[], int i, int s) {
+    //    (R) ------------------------------> 1
     if (i == s) { // condicao terminal
+        // (M)  (D) ------------------------> 2
         return A[i];
-    } else {
-        int m = (i + s) / 2;
-        int maior1 = maxVetor_(A, i, m);
-        int maior2 = maxVetor_(A, m+1, s);
+    } else { // fase ativa
+        int m, maior1, maior2;
+        // (M) (A) (A) ---------------------> 3
+        m = (i + s) / 2;
+        //    (M)   (*) --------------------> 1 + T(n/2)
+        maior1 = maxVetor_(A, i, m);
+        //    (M)   (*)       (A) ----------> 2 + T(n/2)
+        maior2 = maxVetor_(A, m+1, s);
+        //        (R) ----------------------> 1
         if (maior1 > maior2) {
+            // (M) -------------------------> 1
             return maior1;
         } else {
+            // (contabiliza if OU else) ----> X
             return maior2;
         }
-    }
-}
+    } // -----------------------------------> ==================
+} // ---------------------------------------> T(n) = 9 + 2T(n/2)
+
+/* Equacao de recorrencia:
+        | 3, se n = 1 (*condicao terminal*)
+T(n) = <
+        | 2T(n/2) + 9, se n > 1 (*fase ativa*)
+
+   Resolucao pelo metodo da expansao telescopica (busca pela formula geral):
+   T(n) = 2[T(n/2)] + 9
+        = 2[2{T(n/4)} + 9] + 9
+        = 2[2{2T(n/8) + 9} + 9] + 9
+        = ...
+        = 2^i.T(n/2^i) + 9(1 + 2 + 4 + ... + 2^i) ==> FORMULA GERAL (provisoria)
+
+   A soma (1 + 2 + 4 + ... + 2^(i-1)) eh o somatorio de uma PG, onde: a1 = 1;
+   q = 2; e n = i (expoentes de 2 indo de 0 a i-1)
+   S = a1.(q^n - 1)/(q - 1) = 1.(2^i - 1)/(2 - 1) = 2^i - 1
+
+   Entao: T(n) = 2^i.T(n/2^i) + 9(2^i - 1) ==> Eq. de recorrencia GEOMETRICA
+
+   Se a formula eh geral, pode-se concluir que T(n/2^i) chegarah ah condicao
+   T(1) em algum momento, no qual: n/2^i = 1, portanto quando 2^i = n
+   Substituindo:
+
+   T(n) = n.T(n/n) + 9(n - 1)
+        = n.T(1) + 9(n-1) = 3n + 9n - 9
+
+   Expressao final:
+   T(n) = 12n - 9 =====> FUNCAO DE COMPLEXIDADE, portanto o algoritmo eh O(n)
+*/
 
 int maxVetor(const int A[], size_t n) {
     return maxVetor_(A, 0, n-1);
